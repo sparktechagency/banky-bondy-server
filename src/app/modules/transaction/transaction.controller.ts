296cbe0d35d3545df226dea1439b8eaec720ff2a
manik <maniksarker652@gmail.com>
2025-09-28 14:51:21 +0600

    make some changes

--- src/app/modules/transaction/transaction.controller.ts
+++ src/app/modules/transaction/transaction.controller.ts
@@ -2,19 +2,19 @@ import httpStatus from 'http-status';
 import catchAsync from '../../utilities/catchasync';
 import sendResponse from '../../utilities/sendResponse';
 import TransactionService from './transaction.service';
-
+///
 const getAllTransaction = catchAsync(async (req, res) => {
-  const result = await TransactionService.getAllTransaction(req.query);
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: 'Transaction retrieved successfully',
-    data: result,
-  });
+    const result = await TransactionService.getAllTransaction(req.query);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Transaction retrieved successfully',
+        data: result,
+    });
 });
 
 const TransactionController = {
-  getAllTransaction,
+    getAllTransaction,
 };
 
 export default TransactionController;
