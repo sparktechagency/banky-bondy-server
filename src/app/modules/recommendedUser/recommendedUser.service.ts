0d80bd8c869f3e369c8f5bd2b52e206b799ea0c3
manik <maniksarker652@gmail.com>
2025-11-06 10:07:56 +0600

    create service for recommended users

--- src/app/modules/recommendedUser/recommendedUser.service.ts
+++ src/app/modules/recommendedUser/recommendedUser.service.ts
@@ -1,21 +1,10 @@
-import httpStatus from "http-status";
-import AppError from "../../error/appError";
-import { IRecommendedUser } from "./recommendedUser.interface";
-import recommendedUserModel from "./recommendedUser.model";
+import { IRecommendedUser } from './recommendedUser.interface';
+import { RecommendedUser } from './recommendedUser.model';
 
-const updateUserProfile = async (id: string, payload: Partial<IRecommendedUser>) => {
-    if (payload.email || payload.username) {
-        throw new AppError(httpStatus.BAD_REQUEST, "You cannot change the email or username");
-    }
-    const user = await recommendedUserModel.findById(id);
-    if (!user) {
-        throw new AppError(httpStatus.NOT_FOUND, "Profile not found");
-    }
-    return await recommendedUserModel.findByIdAndUpdate(id, payload, {
-        new: true,
-        runValidators: true,
-    });
+const createRecommendedUsers = async (users: IRecommendedUser[]) => {
+    const result = await RecommendedUser.insertMany(users);
+    return result;
 };
 
-const RecommendedUserServices = { updateUserProfile };
+const RecommendedUserServices = { createRecommendedUsers };
 export default RecommendedUserServices;
